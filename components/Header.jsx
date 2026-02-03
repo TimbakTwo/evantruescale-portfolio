3ec282def098fbb6bf267035a180d99f0deb1abf
TimbakTwo <hilencesilence@gmail.com>
2026-02-03 21:55:19 +0800

    Fix for header's z-index position

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,20 +8,20 @@ import { useMediaQuery } from "utils";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 
 export function AppHeader() {
-	const isMobile = useMediaQuery();
+  const isMobile = useMediaQuery();
 
-	return (
-		<header className="pt-5 pb-5 sticky top-0 z-10 bg-inherit shadow-sm">
-			<div className="container-md">
-				<div className="flex justify-between items-center gap-3">
-					<Logo />
-					{isMobile ? <MobileMenu /> : <Menu />}
-					<div className="flex items-center gap-5">
-						{!isMobile && <ConnectMedia />}
-						<ThemeSwitcher />
-					</div>
-				</div>
-			</div>
-		</header>
-	);
+  return (
+    <header className="pt-5 pb-5 sticky top-0 z-20 bg-inherit shadow-sm">
+      <div className="container-md">
+        <div className="flex justify-between items-center gap-3">
+          <Logo />
+          {isMobile ? <MobileMenu /> : <Menu />}
+          <div className="flex items-center gap-5">
+            {!isMobile && <ConnectMedia />}
+            <ThemeSwitcher />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
